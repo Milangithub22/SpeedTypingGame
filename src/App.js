5dfd73bc08699170e833c01e47d82cf41bf2edd6
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-08-22 17:59:53 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const STARTING_TIME = 0;
+  const [keystroke, setKeystroke] = useState("");
+  const [countdown, setCountdown] = useState(STARTING_TIME);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (start && countdown > 0) {
+      setTimeout(() => {
+        setCountdown((item) => item - 1);
+      }, 1000);
+    } else if (countdown === 0) {
+      setStart(false);
+    }
+  }, [countdown, start]);
+
+  function key(event) {
+    setKeystroke(event.target.value);
+  }
+  function calculateWords(arr) {
+    const words = arr.trim().split(" ");
+    const counted = words.filter((word) => word !== "").length;
+    return counted.length;
+  }
+  function StartAgain() {
+    setStart(true);
+    setCountdown(STARTING_TIME);
+    setKeystroke("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea value={keystroke} onChange={key()} disabled={!countdown} />
+      <h4>Time reminaing: {countdown}</h4>
+      <button onClick={StartAgain} disabled={countdown}>
+        Start
+      </button>
+      <h1>Word count: {countdown === 0 ? calculateWords(keystroke) : "0"}</h1>
     </div>
   );
 }
-
-export default App;
