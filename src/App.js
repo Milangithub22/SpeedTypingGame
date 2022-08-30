4d83c82f8870527a825829e3fa8744d54411a61c
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-08-30 16:32:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react"
+import Logic from "./customHooks.js"
 
-export default function App() {
-  const STARTING_TIME = 0;
-  const [keystroke, setKeystroke] = useState("");
-  const [countdown, setCountdown] = useState(STARTING_TIME);
-  const [start, setStart] = useState(false);
 
-  useEffect(() => {
-    if (start && countdown > 0) {
-      setTimeout(() => {
-        setCountdown((item) => item - 1);
-      }, 1000);
-    } else if (countdown === 0) {
-      setStart(false);
-    }
-  }, [countdown, start]);
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setKeystroke(value);
-  }
-  function calculateWords(arr) {
-    const words = arr.trim().split(" ");
-    const counted = words.filter((word) => word !== "").length;
-    return counted.length;
-  }
-  function StartAgain() {
-    setStart(true);
-    setCountdown(STARTING_TIME);
-    setKeystroke("");
-  }
-
-  return (
-    <div>
-      <h1>How fast do you type?</h1>
-      <textarea
-        value={keystroke}
-        onChange={handleChange}
-        disabled={!countdown}
-      />
-      <h4>Time reminaing: {countdown}</h4>
-      <button onClick={StartAgain} disabled={countdown}>
-        Start
-      </button>
-      <h1>Word count: {countdown === 0 ? calculateWords(keystroke) : "0"}</h1>
-    </div>
-  );
+function App() {
+    const {wordCount,
+    textBoxRef,
+     handleChange,
+      text,
+       isTimeRunning,
+        timeRemaining,
+         startGame} = Logic()
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
+
+export default App
