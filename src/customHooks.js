e663b27cca2ffb4afcffde626fc04cc060e65175
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-09-14 20:03:25 +0200

    d

--- src/customHooks.js
+++ src/customHooks.js
@@ -1,47 +1,49 @@
-import {useState, useEffect, useRef} from "react"
+import { useState, useEffect, useRef } from "react";
 
-export default function Logic () {
-    const STARTING_TIME = 5
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
+export default function Logic() {
+  const STARTING_TIME = 5;
+
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+  const textBoxRef = useRef(null);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+    textBoxRef.current.disabled = false;
+    textBoxRef.current.focus();
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false);
+      setWordCount(calculateWordCount(text));
     }
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-    
-    
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-        }
-    }, [timeRemaining, isTimeRunning])
-    
-    return {wordCount,textBoxRef, handleChange, text, isTimeRunning, timeRemaining, startGame}
+  }, [timeRemaining, isTimeRunning, text]);
+
+  return {
+    wordCount,
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+  };
 }
-
- 
